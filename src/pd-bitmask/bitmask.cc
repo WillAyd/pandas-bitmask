823d9d407e0e0cc086a28b1ca7837f1c16b05760
Will Ayd <william.ayd@icloud.com>
2024-08-02 15:26:25 -0400

    Use pImpl

--- src/pd-bitmask/bitmask.cc
+++ src/pd-bitmask/bitmask.cc
@@ -11,90 +11,84 @@ using np_arr_type = nb::ndarray<nb::numpy, uint8_t, nb::shape<-1>>;
 
 class BitmaskArray {
 public:
+  // We use a pImpl for anything that can be implemented without
+  // the Python runtime. This makes it easier to test at a low level
+  // and use tools like ASAN
+  std::unique_ptr<BitmaskArrayImpl> pImpl_;
+
+  explicit BitmaskArray(BitmaskArrayImpl &&bmi)
+      : pImpl_(std::make_unique<BitmaskArrayImpl>(std::move(bmi))) {}
+
   explicit BitmaskArray(np_arr_type np_array) {
-    impl_ = std::make_unique<BitmaskArrayImpl>(BitmaskArrayImpl());
+    pImpl_ = std::make_unique<BitmaskArrayImpl>(BitmaskArrayImpl());
     const auto vw = np_array.view();
     const auto nelems = vw.shape(0);
 
-    ArrowBitmapInit(impl_->bitmap_.get());
-    NANOARROW_THROW_NOT_OK(ArrowBitmapReserve(impl_->bitmap_.get(), nelems));
+    ArrowBitmapInit(pImpl_->bitmap_.get());
+    NANOARROW_THROW_NOT_OK(ArrowBitmapReserve(pImpl_->bitmap_.get(), nelems));
     ArrowBitmapAppendInt8Unsafe(
-        impl_->bitmap_.get(), reinterpret_cast<const int8_t *>(np_array.data()),
-        nelems);
+        pImpl_->bitmap_.get(),
+        reinterpret_cast<const int8_t *>(np_array.data()), nelems);
   }
 
   explicit BitmaskArray(nanoarrow::UniqueBitmap &&bitmap)
-      : impl_(std::make_unique<BitmaskArrayImpl>(
+      : pImpl_(std::make_unique<BitmaskArrayImpl>(
             BitmaskArrayImpl(std::move(bitmap)))) {}
-
-  auto Length() const noexcept -> Py_ssize_t { return impl_->Length(); }
-  auto SetItem(Py_ssize_t index, bool value) const -> void {
-    impl_->SetItem(index, value);
-  }
-  auto GetItem(Py_ssize_t index) const -> bool { return impl_->GetItem(index); }
-  auto Invert() const noexcept -> BitmaskArray {
-    return BitmaskArray(impl_->Invert());
-  }
-  auto And(const BitmaskArray &other) const -> BitmaskArray {
-    return BitmaskArray(impl_->BinaryOp(*other.impl_.get(), std::bit_and()));
-  }
-  auto Or(const BitmaskArray &other) const -> BitmaskArray {
-    return BitmaskArray(impl_->BinaryOp(*other.impl_.get(), std::bit_or()));
-  }
-
-  auto XOr(const BitmaskArray &other) const -> BitmaskArray {
-    return BitmaskArray(impl_->BinaryOp(*other.impl_.get(), std::bit_xor()));
-  }
-
-  auto Size() const noexcept -> Py_ssize_t { return impl_->Size(); }
-  auto NBytes() const noexcept -> Py_ssize_t { return impl_->NBytes(); }
 
   auto Bytes() const {
     auto py_bytearray = PyByteArray_FromStringAndSize(
-        reinterpret_cast<const char *>(impl_->bitmap_->buffer.data),
-        impl_->bitmap_->buffer.size_bytes);
+        reinterpret_cast<const char *>(pImpl_->bitmap_->buffer.data),
+        pImpl_->bitmap_->buffer.size_bytes);
     auto bytearray = nb::steal(py_bytearray);
     auto py_bytes = PyBytes_FromObject(bytearray.ptr());
     return nb::steal(py_bytes);
   }
 
-  auto DType() const noexcept -> const char * { return "bool"; }
-  auto Any() const noexcept -> bool { return impl_->Any(); }
-  auto All() const noexcept -> bool { return impl_->All(); }
-  auto Sum() const noexcept -> Py_ssize_t { return impl_->Sum(); }
-
-  auto Copy() const noexcept -> BitmaskArray {
-    return BitmaskArray(impl_->Copy());
-  }
-
   auto NdArray() const noexcept -> np_arr_type {
-    const auto nelems = this->Length();
+    const auto nelems = pImpl_->Length();
     bool *data = new bool[nelems];
-    ArrowBitsUnpackInt8(impl_->bitmap_->buffer.data, 0, nelems,
+    ArrowBitsUnpackInt8(pImpl_->bitmap_->buffer.data, 0, nelems,
                         reinterpret_cast<int8_t *>(data));
     nb::capsule owner(data, [](void *p) noexcept { delete[] (bool *)p; });
 
     size_t shape[1] = {static_cast<size_t>(nelems)};
     return np_arr_type(data, 1, shape, owner);
   }
-
-private:
-  explicit BitmaskArray(BitmaskArrayImpl &&bmi)
-      : impl_(std::make_unique<BitmaskArrayImpl>(std::move(bmi))) {}
-
-  std::unique_ptr<BitmaskArrayImpl> impl_;
 };
 
 NB_MODULE(bitmask, m) {
   nb::class_<BitmaskArray>(m, "BitmaskArray")
       .def(nb::init<np_arr_type>())
-      .def("__len__", &BitmaskArray::Length)
-      .def("__setitem__", &BitmaskArray::SetItem)
-      .def("__getitem__", &BitmaskArray::GetItem)
-      .def("__invert__", &BitmaskArray::Invert)
-      .def("__and__", &BitmaskArray::And)
-      .def("__or__", &BitmaskArray::Or)
-      .def("__xor__", &BitmaskArray::XOr)
+      .def(
+          "__len__",
+          [](const BitmaskArray &bma) noexcept { return bma.pImpl_->Length(); })
+      .def("__setitem__",
+           [](const BitmaskArray &bma, Py_ssize_t index, bool value) {
+             return bma.pImpl_->SetItem(index, value);
+           })
+      .def("__getitem__",
+           [](const BitmaskArray &bma, Py_ssize_t index) {
+             return bma.pImpl_->GetItem(index);
+           })
+      .def("__invert__",
+           [](const BitmaskArray &bma) noexcept {
+             return BitmaskArray(bma.pImpl_->Invert());
+           })
+      .def("__and__",
+           [](const BitmaskArray &bma, const BitmaskArray &other) {
+             return BitmaskArray(
+                 bma.pImpl_->BinaryOp(*other.pImpl_.get(), std::bit_and()));
+           })
+      .def("__or__",
+           [](const BitmaskArray &bma, const BitmaskArray &other) {
+             return BitmaskArray(
+                 bma.pImpl_->BinaryOp(*other.pImpl_.get(), std::bit_or()));
+           })
+      .def("__xor__",
+           [](const BitmaskArray &bma, const BitmaskArray &other) {
+             return BitmaskArray(
+                 bma.pImpl_->BinaryOp(*other.pImpl_.get(), std::bit_xor()));
+           })
       .def("__getstate__",
            [](const BitmaskArray &bma) { return bma.NdArray(); })
       .def("__setstate__",
@@ -103,15 +97,26 @@ NB_MODULE(bitmask, m) {
            })
       //.def("__iter__",
       //.def("concatenate",
-      .def_prop_ro("size", &BitmaskArray::Size)
-      .def_prop_ro("nbytes", &BitmaskArray::NBytes)
+      .def_prop_ro(
+          "size",
+          [](const BitmaskArray &bma) noexcept { return bma.pImpl_->Size(); })
+      .def_prop_ro(
+          "nbytes",
+          [](const BitmaskArray &bma) noexcept { return bma.pImpl_->NBytes(); })
       .def_prop_ro("bytes", &BitmaskArray::Bytes)
       //.def_prop_ro("shape", &BitmaskArray::Shape)
-      .def_prop_ro("dtype", &BitmaskArray::DType)
-      .def("any", &BitmaskArray::Any)
-      .def("all", &BitmaskArray::All)
-      .def("sum", &BitmaskArray::Sum)
+      .def_prop_ro("dtype",
+                   [](const BitmaskArray &bma) noexcept { return "bool"; })
+      .def("any",
+           [](const BitmaskArray &bma) noexcept { return bma.pImpl_->Any(); })
+      .def("all",
+           [](const BitmaskArray &bma) noexcept { return bma.pImpl_->All(); })
+      .def("sum",
+           [](const BitmaskArray &bma) noexcept { return bma.pImpl_->Sum(); })
       //.def("take_1d", &BitmaskArray::Take1D)
-      .def("copy", &BitmaskArray::Copy)
+      .def("copy",
+           [](const BitmaskArray &bma) noexcept {
+             return BitmaskArray(bma.pImpl_->Copy());
+           })
       .def("__array__", &BitmaskArray::NdArray);
 }
